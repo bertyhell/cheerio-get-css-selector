54372dbcb76c00f573527ea1e5ed4c054a5b6a3c
Luke Bennett <LukeeeeBennettPlus@gmail.com>
2018-01-19 21:53:06 +0000

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,13 +1,13 @@
-var $ = require('cheerio');
+var cheerio = require('cheerio');
 var chai = require('chai');
 var expect = chai.expect;
 var fs = require('fs');
 
-var getUniqueSelector = require('../src/get-unique-selector.js').init();
+var GetUniqueSelector = require('../src/get-unique-selector.js')
 
-function compareElementsForSelector($html, classSelector) {
-  var findUsingUniqueSelector = $html.find($.getUniqueSelector($html.find(classSelector)));
-  var findUsingClass          = $html.find(classSelector);
+function compareElementsForSelector($, classSelector) {
+  var findUsingClass          = $(classSelector);
+  var findUsingUniqueSelector = $(findUsingClass.getUniqueSelector());
   var isSameElement           = findUsingClass.is(findUsingUniqueSelector);
   expect(isSameElement).to.be.true;
   expect(findUsingUniqueSelector).to.have.lengthOf(1); // Only one selected element
@@ -15,38 +15,38 @@ function compareElementsForSelector($html, classSelector) {
 
 describe('Get Unique Selector', () => {
   var htmlString = fs.readFileSync('./test/fixtures/example.html', 'utf8');
-  var $html = $(htmlString);
+  var $ = cheerio.load(htmlString);
 
   it('should get the unique selector for normal LI', () => {
 
-    compareElementsForSelector($html, '.test1');
+    compareElementsForSelector($, '.test1');
   });
 
   it('should get the unique selector for element with ID', () => {
-    compareElementsForSelector($html, '.test2');
+    compareElementsForSelector($, '.test2');
   });
 
   it('should get the unique selector for element with ID ANCESTOR', () => {
-    compareElementsForSelector($html, '.test3');
+    compareElementsForSelector($, '.test3');
   });
 
   it('should get the unique selector for BODY', () => {
-    compareElementsForSelector($html, '.test4');
+    compareElementsForSelector($, '.test4');
   });
 
   it('should get the unique selector for span with id under other id', () => {
-    compareElementsForSelector($html, '.test5');
+    compareElementsForSelector($, '.test5');
   });
 
   it('should get the unique selector for span under multiple anchestors with an id', () => {
-    compareElementsForSelector($html, '.test6');
+    compareElementsForSelector($, '.test6');
   });
 
   it('should get the unique selector for the first span', () => {
-    compareElementsForSelector($html, '.test7');
+    compareElementsForSelector($, '.test7');
   });
 
   it('should get the unique selector using the :last-child selector', () => {
-    compareElementsForSelector($html, '.test8');
+    compareElementsForSelector($, '.test8');
   });
 });
